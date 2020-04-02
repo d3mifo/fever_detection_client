660c0c28134b6bed4f9966a9212dbd48b046d371
Ford <awsford@a483e709a56e.ant.amazon.com>
2020-04-02 11:46:04 +0100

    added AWS Amplify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Amplify from 'aws-amplify';
+
+import './styles/index.css';
+import App from './components/App';
+import config from './config';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        idendityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        idendityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: "",
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            }
+        ]
+    }
+})
 
 ReactDOM.render(
     <Router>
