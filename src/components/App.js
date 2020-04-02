f3bf2e41ae93e9ee615d27a8a7eb04dbff59bb16
Ford <awsford@a483e709a56e.ant.amazon.com>
2020-04-02 11:27:35 +0100

    Added basic routing and Home container

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Navbar } from 'react-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, NavDropdown, NavItem } from 'react-bootstrap';
+
+import Routes from '../Routes';
+
+
 import '../styles/App.css';
 
 class App extends React.Component {
     render() {
         return (
-            // <div className="App container">
-            <Container>
-                <Navbar fixed="top" bg="light" expand="lg">
-                    <Navbar.Brand>
-                        <Link to="/">Dashboard</Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse title="Dropdown"></Navbar.Collapse>
+            <div className="App container">
+                <Navbar sticky="top" collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Navbar.Brand><Link to="/">React-Bootstrap</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#features">X</Nav.Link>
+                            <Nav.Link href="#pricing">Y</Nav.Link>
+                            <NavDropdown title="zDrop" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <LinkContainer to="/signup">
+                                <Nav.Link>Signup</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>Login</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
-            {/* </div> */}
-            </Container>
+                <Routes />
+            </div>
         );
     }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Home.css';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="lander">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Home;
