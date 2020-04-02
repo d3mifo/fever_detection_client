2f1f51d579ed298aa0d5dc09e3a9faca357d09b9
Ford <awsford@a483e709a56e.ant.amazon.com>
2020-04-02 12:23:52 +0100

    Added login functionality

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
+import Login from './components/Login';
 import NotFound from './components/NotFound';
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
             <Route component={NotFound} />
         </Switch>
     )
---
+++ src/components/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Auth } from 'aws-amplify'
+
+import '../styles/Login.css'
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: "", password: "" }
+    }
+
+    validateForm = () => {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            await Auth.signIn(this.state.email, this.state.password);
+            alert("Logged In");
+        } catch(e) {
+            alert(e.message)
+        }
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control autoFocus type="email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
+                    </Form.Group>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })} />
+                    </Form.Group>
+                    <Button block size="large" disabled={!this.validateForm()} type="submit">Login</Button>
+                </Form>
+            </div>
+        )
+    }
+}
